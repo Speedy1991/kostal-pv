ae0129e158d8d414058e1971cdc6d00b734ef1b1
Arthur Bayr <arthur.holz.91@gmail.com>
2021-03-20 19:53:52 +0100

    let it fail and restart service

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 from ksem import KSEM
 from piko15 import Piko15
 from influx_client import InfluxClient
-import time
 
 
 def create_points(instance):
     points = []
-    try:
-        meta_tags = instance.tags
-        for result in instance.get_results():
-            point_tags = result['tags']
-            point_tags.update(meta_tags)
-            fields = dict(value=result['value'])
-            fields.update({
-                '_'.join(point_tags.values()): result['value']
-            })
-            point = InfluxClient.create_idb_point(
-                measurement_name=result.get('name', None),
-                tags=point_tags,
-                fields=fields,
-                ts=result['ts']
-            )
-            points.append(point)
-    except Exception as e:
-        print(e)
-    finally:
-        return points
+    meta_tags = instance.tags
+    for result in instance.get_results():
+        point_tags = result['tags']
+        point_tags.update(meta_tags)
+        fields = dict(value=result['value'])
+        fields.update({
+            '_'.join(point_tags.values()): result['value']
+        })
+        point = InfluxClient.create_idb_point(
+            measurement_name=result.get('name', None),
+            tags=point_tags,
+            fields=fields,
+            ts=result['ts']
+        )
+        points.append(point)
+    return points
 
 
 def run():
@@ -34,14 +29,10 @@ def run():
     inverter = Piko15()
 
     while True:
-        try:
-            points = [*create_points(ksem), *create_points(inverter)]
-            if len(points) > 0:
-                client.write_points('PV', points)
-                print(f"[{len(points)}] Points written to influxdb")
-        except Exception as e:
-            print(e)
-            time.sleep(10)
+        points = [*create_points(ksem), *create_points(inverter)]
+        if len(points) > 0:
+            client.write_points('PV', points)
+            print(f"[{len(points)}] Points written to influxdb")
 
 
 if __name__ == '__main__':
